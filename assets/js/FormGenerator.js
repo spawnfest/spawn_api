069e121a19cc076e024b157919bcdf0bd15565e5
Bruce <brucepark0@gmail.com>
2018-11-23 21:48:07 -0800

    Fix indents

--- assets/js/FormGenerator.js
+++ assets/js/FormGenerator.js
@@ -1,57 +1,57 @@
-import React, { Fragment } from "react";
-import { Field, reduxForm } from "redux-form";
-import { Row, Col } from "react-bootstrap";
-import submit from "./Submit";
+import React, { Fragment } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { Row, Col } from 'react-bootstrap'
+import submit from './Submit'
 
 const FormGenerator = props => {
   const { handleSubmit, submitting, fields } = props
-  return(
+  return (
     <Fragment>
-    <form onSubmit={handleSubmit(submit)}>
-    <Row>
-    <Col md={6} key="Field Name" />
-    </Row>
-    <Row>
-    <Col md={6} key="Type" />
-    </Row>
-    <Row>
-    {fields.map(f => (
-      <div key={`${f.name}-${f.type}`}>
-      <Fragment>
-      <Col md={6} key={f.name}>
-      <Field
-      name={f.name}
-      component="input"
-      label={f.name}
-      placeholder={f.name}
-      />
-      </Col>
-      </Fragment>
-      <Fragment>
-      <Col md={6} key={f.type}>
-      <Field
-      name={f.type}
-      component="input"
-      label={f.type}
-      placeholder={f.type}
-      />
-      </Col>
-      </Fragment>
-      </div>
-    ))}
-    </Row>
-    <button type="submit" disabled={submitting}>
+      <form onSubmit={handleSubmit(submit)}>
+        <Row>
+          <Col md={6} key='Field Name' />
+        </Row>
+        <Row>
+          <Col md={6} key='Type' />
+        </Row>
+        <Row>
+          {fields.map(f => (
+            <div key={`${f.name}-${f.type}`}>
+              <Fragment>
+                <Col md={6} key={f.name}>
+                  <Field
+                    name={f.name}
+                    component='input'
+                    label={f.name}
+                    placeholder={f.name}
+                  />
+                </Col>
+              </Fragment>
+              <Fragment>
+                <Col md={6} key={f.type}>
+                  <Field
+                    name={f.type}
+                    component='input'
+                    label={f.type}
+                    placeholder={f.type}
+                  />
+                </Col>
+              </Fragment>
+            </div>
+          ))}
+        </Row>
+        <button type='submit' disabled={submitting}>
     Create Mock Data
-    </button>
-    </form>
+        </button>
+      </form>
     </Fragment>
   )
 }
 
 const FgForm = reduxForm({
-  form: "wizard",
+  form: 'wizard',
   destroyOnUnmount: false,
   forceUnregisterOnUnmount: true
-})(FormGenerator);
+})(FormGenerator)
 
-export default FgForm;
+export default FgForm
