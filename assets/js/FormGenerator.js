61d955e3bc1d6f4120af2fd6907525fbf3b94027
tjhiry29 <r.tjhie0809@gmail.com>
2018-11-23 21:52:40 -0500

    provide redux store to application and add create-redux-store file

--- assets/js/FormGenerator.js
+++ assets/js/FormGenerator.js
@@ -1,47 +1,48 @@
-import React, {Fragment} from 'react'
-import { Field, reduxForm } from 'redux-form'
-import { Row, Col } from 'react-bootstrap'
+import React, { Fragment } from "react";
+import { Field, reduxForm } from "redux-form";
+import { Row, Col } from "react-bootstrap";
 
 const FormGenerator = ({ fields }) => (
   <Fragment>
-  <Row>
-  <Col md={6} key="Field Name"></Col>
-  </Row>
-  <Row>
-  <Col md={6} key="Type"></Col>
-  </Row>
-  <Row>
-  {fields.map(f => (
-    <div>
-    <Fragment>
-    <Col md={6} key={f.name}>
-    <Field
-    name={f.name}
-    component="input"
-    label={f.name}
-    placeholder={f.name}>
-    </Field>
-    </Col>
-    </Fragment>
-    <Fragment>
-    <Col md={6} key={f.type}>
-    <Field
-    name={f.type}
-    component="input"
-    label={f.type}
-    placeholder={f.type}></Field>
-    </Col>
-    </Fragment>
-    </div>
-  ))}
-  </Row>
+    <Row>
+      <Col md={6} key="Field Name" />
+    </Row>
+    <Row>
+      <Col md={6} key="Type" />
+    </Row>
+    <Row>
+      {fields.map(f => (
+        <div key={`${f.name}-${f.type}`}>
+          <Fragment>
+            <Col md={6} key={f.name}>
+              <Field
+                name={f.name}
+                component="input"
+                label={f.name}
+                placeholder={f.name}
+              />
+            </Col>
+          </Fragment>
+          <Fragment>
+            <Col md={6} key={f.type}>
+              <Field
+                name={f.type}
+                component="input"
+                label={f.type}
+                placeholder={f.type}
+              />
+            </Col>
+          </Fragment>
+        </div>
+      ))}
+    </Row>
   </Fragment>
-)
+);
 
 const FgForm = reduxForm({
-  form: 'wizard',
+  form: "wizard",
   destroyOnUnmount: false,
   forceUnregisterOnUnmount: true
-})(FormGenerator)
+})(FormGenerator);
 
-export default FgForm
+export default FgForm;
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from "../css/app.css";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,7 +9,7 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
@@ -20,32 +20,37 @@ import "phoenix_html"
 
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import FgForm from "./FormGenerator";
+import createReduxStore from "./create-redux-store";
+
+const store = createReduxStore();
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formFields: [{name: "email", type: "string"}]
-    }
+      formFields: [{ name: "email", type: "string" }]
+    };
   }
 
   render() {
     return (
       <Fragment>
-      <FgForm fields={this.state.formFields}/>
+        <FgForm fields={this.state.formFields} />
       </Fragment>
     );
   }
 }
 
 ReactDOM.render(
-  <Router>
-  <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("spawn-app")
 );
 
 export default App;
-
