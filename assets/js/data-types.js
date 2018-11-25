6384a53c11f5cc07f70fbca41e01f6c6ac1248ac
tjhiry29 <r.tjhie0809@gmail.com>
2018-11-25 15:49:49 -0500

    add ip address and UUID options

--- assets/js/data-types.js
+++ assets/js/data-types.js
@@ -3,7 +3,17 @@ const DATA_TYPES = [
   { name: "First Name", value: "first_name" },
   { name: "Last Name", value: "last_name" },
   { name: "Number", value: "integer" },
-  { name: "Float", value: "float" }
+  { name: "Float", value: "float" },
+  { name: "IP v4 Address", value: "ip_v4_address" },
+  { name: "IP v6 Address", value: "ip_v6_address" },
+  { name: "City", value: "city" },
+  { name: "Country", value: "country" },
+  { name: "Latitude", value: "latitude" },
+  { name: "Longitude", value: "longitude" },
+  { name: "Time Zone", value: "time_zone" },
+  { name: "Zip Code", value: "zip_code" },
+  { name: "Country Code", value: "country_code" },
+  { name: "UUID", value: "uuid" }
 ];
 
 export default DATA_TYPES;
