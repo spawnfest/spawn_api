986ce6a682445a93739634f350a1955027d86f8d
Bruce <brucepark0@gmail.com>
2018-11-24 16:31:21 -0800

    Remove event

--- assets/js/Submit.js
+++ assets/js/Submit.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 function submit(values) {
-  event.preventDefault();
   let schemaVals = values.fields.reduce((accumulator, f, index) => {
     let obj = {};
     obj[f] = values.types[index];
