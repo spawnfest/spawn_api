b02551e322623a458463e203b35368475c0cac30
Bruce <brucepark0@gmail.com>
2018-11-24 16:12:22 -0800

    Update to functional

--- assets/js/FormGenerator.js
+++ assets/js/FormGenerator.js
@@ -2,40 +2,38 @@ import React, { Fragment, Component } from "react";
 import { Field, FieldArray, reduxForm } from "redux-form";
 import { Row, Col } from "react-bootstrap";
 import submit from "./Submit";
-class renderSchemaFields extends Component {
-  render() {
-    const { fields } = this.props;
-    return (
-      <Row>
-        {fields.map((f, index) => (
-          <span key={index}>
-            <Col md={4} label="Field Name">
-              Field Name
-              <Field
-                name={`fields[${index}]`}
-                type="text"
-                component="input"
-                label={f.name}
-                placeholder={f.name}
-              />
-            </Col>
-            <Col md={3} /> &nbsp;
-            <Col md={4} label="Field Type">
-              Field Type
-              <Field
-                name={`types[${index}]`}
-                type="text"
-                component="input"
-                label={f.type}
-                placeholder={f.type}
-              />
-            </Col>
-          </span>
-        ))}
-      </Row>
-    );
-  }
-}
+
+const RenderSchemaFields = ({ fields }) => {
+  return (
+    <Row>
+      {fields.map((f, index) => (
+        <span key={index}>
+          <Col md={4} label="Field Name">
+            Field Name
+            <Field
+              name={`fields[${index}]`}
+              type="text"
+              component="input"
+              label={f.name}
+              placeholder={f.name}
+            />
+          </Col>
+          <Col md={3} /> &nbsp;
+          <Col md={4} label="Field Type">
+            Field Type
+            <Field
+              name={`types[${index}]`}
+              type="text"
+              component="input"
+              label={f.type}
+              placeholder={f.type}
+            />
+          </Col>
+        </span>
+      ))}
+    </Row>
+  );
+};
 
 const FormGenerator = props => {
   const { handleSubmit, submitting, fields, addRowHandler } = props;
@@ -44,7 +42,7 @@ const FormGenerator = props => {
       <FieldArray
         name="schemaFields"
         props={{ fields: fields }}
-        component={renderSchemaFields}
+        component={RenderSchemaFields}
       />
       <button
         type="button"
