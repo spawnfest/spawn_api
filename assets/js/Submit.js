85f00dd62ef72870b8227e048d43b71270d96947
Bruce <brucepark0@gmail.com>
2018-11-24 16:14:56 -0800

    update

--- assets/js/Submit.js
+++ assets/js/Submit.js
@@ -3,17 +3,17 @@ import axios from "axios";
 function submit(values) {
   event.preventDefault();
   let schemaVals = values.fields.reduce((accumulator, f, index) => {
-    let obj = {}
-    obj[f] = values.types[index]
-    return Object.assign(accumulator, obj)}, {});
-    axios
+    let obj = {};
+    obj[f] = values.types[index];
+    return Object.assign(accumulator, obj);
+  }, {});
+  axios
     .post(API_URL, {
       schema: schemaVals
     })
     .then(response => {
       console.log(response);
     });
-};
+}
 
-
-export default submit
+export default submit;
